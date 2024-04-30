9fa8ab851f0c702e7f5e348c2e31350ebe080715
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-04-30 12:14:07 +1000

    server completed to check if login is correct or not

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { insertUser } from "../models/user/UserModel.js";
+import { comparePassword, hasPassword } from "../utils/bcryptjs.js";
 const router = express.Router();
 
 // router = {
@@ -21,6 +22,7 @@ router.get("/", (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
+    req.body.password = hasPassword(req.body.password);
     const result = await insertUser(req.body);
 
     result?._id
@@ -39,6 +41,38 @@ router.post("/", async (req, res) => {
       error.message =
         "There is already another account associated to this email. Use different email to signup.";
     }
+    res.status(code).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // get the user email and get user from db
+    const user = await getUserByEmail(email);
+
+    if (user?._id) {
+      //compare password
+
+      const isMatched = comparePassword(password, user.password);
+
+      if (isMatched) {
+        //authorized
+        return res.json({
+          status: "success",
+          message: "Login sucessfully",
+        });
+      }
+    }
+    res.json({
+      status: "error",
+      message: "Invalid credentials",
+    });
+  } catch (error) {
+    let code = 500;
     res.status(code).json({
       status: "error",
       message: error.message,
